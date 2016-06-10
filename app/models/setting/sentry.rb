c4cd4dc7ef53318ed101bdcc5336ba51271ca907
Brandon Weeks <weeks@squareup.com>
2016-06-10 11:37:34 -0700

    Correctly create the setting with a blank default (#1)

--- app/models/setting/sentry.rb
+++ app/models/setting/sentry.rb
@@ -2,11 +2,12 @@ class Setting::Sentry < ::Setting
   def self.load_defaults
     return unless super
 
-    # FIXME: this should work with an empty string
+    # This is required to create a new Setting::Sentry with :default set to a blank string
+    Setting::BLANK_ATTRS.append 'sentry_dsn'
     Setting.transaction do
       [
-        set('sentry_dsn', N_('The DSN of your Sentry server'), 'asdf', N_('Sentry DSN'))
-      ].compact.each { |s| create s.update(category: 'Setting::Sentry') }
+        set('sentry_dsn', N_('The DSN of your Sentry server'), '', N_('Sentry DSN'))
+      ].compact.each { |s| create! s.update(category: 'Setting::Sentry') }
     end
 
     true
